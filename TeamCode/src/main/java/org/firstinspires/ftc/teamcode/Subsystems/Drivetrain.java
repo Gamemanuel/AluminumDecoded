9272caf9a46431078207d6d153312d0f7184a82e
gamemanuel <themathnight@gmail.com>
2026-02-02 11:22:21 -0700

    removed mechanum drive from Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -12,7 +12,7 @@ public class Drivetrain {
     public DcMotorEx frontLeft, backLeft, frontRight, backRight;
     public MotorGroup leftSide, rightSide;
     public SixWheelCMD cmd;
-    public Imu imu;
+
     public Drivetrain(HardwareMap hMap) {
         // We are defining the motors here:
         // Left side of the robot
@@ -42,23 +42,5 @@ public class Drivetrain {
     public void tankDrive(double left, double right) {
         cmd.setMotors(left, right);
     }
-    public void mecanumDrive(double x, double y, double turn) {
-        double robotHeading = imu.getRobotHeading();
 
-        //Rotate the movement direction counter to the bot's rotation
-        double driveRotation = (x) * Math.sin(-robotHeading) + (y) * Math.cos(-robotHeading);
-        double strafeRotation = (x) * Math.cos(-robotHeading) - (y) * Math.sin(-robotHeading);
-
-        double maxMotorPower = Math.abs(driveRotation) + Math.abs(strafeRotation) + Math.abs(x);
-        double denominator = Math.max(maxMotorPower, 1);
-        // If this wasn't here the robot would not stay straight, it actually has some weird
-        // behavior where it tries to point at the center of the field. Comment out to see.
-
-        frontLeft.setPower((driveRotation + strafeRotation + turn) / denominator);
-        frontRight.setPower((driveRotation - strafeRotation - turn) / denominator);
-        backLeft.setPower((driveRotation - strafeRotation + turn) / denominator);
-        backRight.setPower((driveRotation + strafeRotation - turn) / denominator);
-        // Don't use motor groups when dealing with mecanum. It literally defeats the purpose of
-        // mecanum (strafing)
-    }
 }
