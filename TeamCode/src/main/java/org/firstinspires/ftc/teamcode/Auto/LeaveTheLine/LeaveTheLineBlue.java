4e55dc15a0a3c816906f34ffde9c4f5add0f4487
gamemanuel <themathnight@gmail.com>
2026-02-06 21:42:28 -0700

    removed the wrappers from leave the line.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeaveTheLine/LeaveTheLineBlue.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auto.LeaveTheLine;
-
-import org.firstinspires.ftc.teamcode.Utils.Alliance;
-
-public class LeaveTheLineBlue extends LeaveTheLine{
-    public LeaveTheLineBlue() { super(Alliance.BLUE); }
-}
