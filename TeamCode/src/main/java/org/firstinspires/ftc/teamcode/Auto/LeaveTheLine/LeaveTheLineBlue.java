02cccaf38b0bfaceb712b0eae1c2721a2b24f643
gamemanuel <themathnight@gmail.com>
2026-02-06 21:29:04 -0700

    removed unneded wrapper files and addde RoadrunnerDriveForward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeaveTheLine/LeaveTheLineBlue.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auto.LeaveTheLine;
-
-public class LeaveTheLineBlue extends LeaveTheLine{
-    public LeaveTheLineBlue() { super(Alliance.BLUE); }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeaveTheLine/LeaveTheLineRed.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auto.LeaveTheLine;
-
-public class LeaveTheLineRed extends LeaveTheLine{
-    public LeaveTheLineRed() { super(Alliance.BLUE);}
-}
